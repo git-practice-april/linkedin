7f7816610fcae57ae6dec4f1b8f7e1aca778d089
priyankabhavanam <priyankatiru.bhavanam@gmail.com>
2022-04-30 02:26:10 +0530

    updated

--- profile.java
+++ profile.java
@@ -1,4 +1,4 @@
 this is from master branch
-this is created to configure  linkedin profile
+this is created to configure  linkedin profile..updated
 now i am modifying this script
 this line added in prod branch
